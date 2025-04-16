7df85bc87ed2ce63803f82672d0446b5bb99d37b
Rushab Shah <rushabshah32@gmail.com>
2025-04-16 15:46:16 +0530

    Update entrypoint.sh

--- simple_dockerfiles/tailscale_docker_ssh/entrypoint.sh
+++ simple_dockerfiles/tailscale_docker_ssh/entrypoint.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# Define cleanup procedure: https://stackoverflow.com/a/41451517/6437140
+cleanup() {
+    echo "Container stopped, performing cleanup..."
+    tailscale logout
+}
+
+# Trap SIGTERM
+trap 'cleanup' SIGTERM
+
+# Execute a command
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
 tailscale up --accept-dns=false --accept-routes=false --hostname ctop --ssh --auth-key "${TS_AUTH_KEY}"
-sleep infinity
+
+echo "TS Initialized"
+
+sleep infinity &
+
+# Wait
+wait $!
